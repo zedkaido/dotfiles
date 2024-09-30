0ccc6bf65d2b2fbb905bc3417ed4f492c870b211
Zed Kaido <z@zedkaido.com>
2024-09-30 22:13:59 +0100

    [yabai] fix sketchybar volume.sh (when a device has no volume ctrl)

--- yabai/.config/sketchybar/plugins/volume.sh
+++ yabai/.config/sketchybar/plugins/volume.sh
@@ -9,7 +9,7 @@ case $VOLUME in
 	[6-9][0-9]|100) ICON="VOL" ;;
 	[3-5][0-9]) ICON="VOL" ;;
 	[1-9]|[1-2][0-9]) ICON="VOL" ;;
-	*) ICON="VOL::"
+	*) ICON="VOL:: Ꝏ "
 esac
 
 sketchybar --set $NAME icon="$ICON" label="$VOLUME%"
