eff45c79a5c3309600df57845c8c7f4094cb5601
Zed Kaido <z@zedkaido.com>
2024-11-13 22:11:34 +0000

    [yabai, sketchybar] add support for layout info [BSP, FLOAT, STACK]

---
+++ yabai/.config/sketchybar/plugins/layout-type.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+layout_type=$(yabai -m query --spaces --space | jq -r ".type" | tr '[:lower:]' '[:upper:]')
+formatted_layout_type="[${layout_type}]"
+sketchybar --set $NAME label="${formatted_layout_type}"
