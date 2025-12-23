a1ace1a1f0ebe09073506425e2e0513a0fe63225
Zed Kaido <z@zedkaido.com>
2025-12-23 22:04:03 +0000

    [sketchybar] stack -> 1based (1bottom)

--- yabai/.config/sketchybar/scripts/focus.sh
+++ yabai/.config/sketchybar/scripts/focus.sh
@@ -5,5 +5,6 @@ readarray -t arr <<< $(echo "$window" | jq -r '.app, .title, ."stack-index"')
 app=${arr[0]}
 window_title=${arr[1]}
 stackindex=${arr[2]}
+stackindex=$(( stackindex == 0 ? 1 : stackindex ))
 
 sketchybar --set $NAME label="${stackindex:+"::  [$stackindex]"} ${app:+"$app -"} ${window_title:0:55}"
