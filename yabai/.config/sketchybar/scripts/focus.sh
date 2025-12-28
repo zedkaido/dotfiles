c93ee8cfd9df64dd36fbd939c1d9f22dbf316542
Zed Kaido <z@zedkaido.com>
2025-12-28 02:54:18 +0000

    [binaries/skhdrc] keys + dmenu optimization

--- yabai/.config/sketchybar/scripts/focus.sh
+++ yabai/.config/sketchybar/scripts/focus.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
-window=$(yabai -m query --windows --window)
-readarray -t arr <<< $(echo "$window" | jq -r '.app, .title, ."stack-index"')
+window=$(yabai -m query --windows --window 2>/dev/null);
+readarray -t arr < <(echo "$window" | jq -r '.app, .title, ."stack-index", ."has-parent-zoom", ."has-fullscreen-zoom"')
+
 app=${arr[0]}
 window_title=${arr[1]}
 stackindex=${arr[2]}
 stackindex=$(( stackindex == 0 ? 1 : stackindex ))
+if [[ ${arr[3]} = "true" ]]; then
+   zoom="[PZOOM]"
+elif [[ ${arr[4]} = "true" ]]; then
+   zoom="[FZOOM]"
+fi
 
-sketchybar --set $NAME label="${stackindex:+"::  [$stackindex]"} ${app:+"$app -"} ${window_title:0:55}"
+sketchybar --set $NAME label="${stackindex:+"::  [$stackindex]"} $zoom ${app:+"$app -"} ${window_title:0:55}"
